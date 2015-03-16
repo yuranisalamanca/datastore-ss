02046a164f584101b41831f02676192f92344e5d
yuranisalamanca <yuranisalamanca@gmail.com>
2015-03-15 22:20:19 -0500

    Proyecto PHP
    Diseño Index y Cuenta

---
+++ js/indexjs.js
@@ -0,0 +1,29 @@
+function cambiarTab(tabs,tabActual){
+	//id del tab actual (tab2)
+	tabActual=document.getElementById(tabActual.id);
+	//tabs
+	listaTabs=document.getElementById(tabs.id);
+	//id del contenido actual
+	ctabActual = document.getElementById('c'+tabActual.id);
+	//contenidotabs (id del div que tiene los contenidos)
+	listaCTabs = document.getElementById('contenido'+tabs.id);
+	i=0;
+
+	while (typeof listaCTabs.getElementsByTagName('div')[i] != 'undefined'){
+        $(document).ready(function(){
+            $(listaCTabs.getElementsByTagName('div')[i]).css('display','none');
+            $(listaTabs.getElementsByTagName('li')[i]).css('background','');
+            $(listaTabs.getElementsByTagName('li')[i]).css('padding-bottom','');
+        });
+	i += 1;
+    }
+    $(document).ready(function(){
+        // Muestra el contenido de la pestaña pasada como parametro a la funcion,
+        // cambia el color de la pestaña y aumenta el padding para que tape el  
+        // borde superior del contenido que esta juesto debajo y se vea de este 
+        // modo que esta seleccionada.
+        $(ctabActual).css('display','');
+        $(tabActual).css('background','rgba(133, 167, 150, 0.35)');
+        $(tabActual).css('padding-bottom','2px'); 
+    });
+}
---
+++ php/dropbox/index.php
@@ -0,0 +1,6 @@
+<?php
+	ini_set('display_errors', 1); 
+	error_reporting(E_ALL);
+	require_once "libs/dropbox-sdk/Dropbox/autoload.php";
+	$appInfo = dbx\AppInfo::loadFromJsonFile("app-info.json"); 
+ ?>
