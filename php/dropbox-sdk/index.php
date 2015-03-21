4aa7e8b83abdf8241f238095aa8279b51c40c7bb
Judaseec <judaseec@hotmail.com>
2015-03-21 17:11:25 -0500

    Automatizacion de ingreso a dropbox

--- php/dropbox-sdk/index.php
+++ php/dropbox-sdk/index.php
@@ -6,30 +6,18 @@ $appInfo = dbx\AppInfo::loadFromJsonFile("app-info.json");
 $webAuth = new dbx\WebAuthNoRedirect($appInfo, "Datastoress");
 $authorizeUrl = $webAuth->start();
 
-echo "1. Go to: " . $authorizeUrl . "\n";
+//echo "1. Go to: " . $authorizeUrl . "\n";
 //echo "2. Click \"Allow\" (you might have to log in first).\n";
 //echo "3. Copy the authorization code.\n";
 //$authCode = \trim(\readline("Enter the authorization code here: "));
-
-/* curl_init(): inicializar una sesión cURL*/
+$authorizeUrl = $authorizeUrl."&redirect_uri=http://localhost/Datastoress/php/recibir.php";
+//$authorizeUrl = $authorizeUrl;
 header("Location: ".$authorizeUrl);
-
-/*$ch = curl_init($authorizeUrl);
-$userAgent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)';
-// add useragent 
-
-/*curl_setopt — Configura una opción para una transferencia cURL
-curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
-/* CURLOPT_RETURNTRANSFER retorna la transferencia como un string
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-/*curl_exec — Establece una sesión cURL
-$output = curl_exec($ch);
-*/
-
-list($accessToken, $dropboxUserId) = $webAuth->finish("5hHUtrKVeowAAAAAAAAO1xYxpMu2NKBTfJQzYX2r_ec");
+/*
+list($accessToken, $dropboxUserId) = $webAuth->finish("S776D67kj0QAAAAAAACH5xXqsljtLP_W4vOgR5kS4Wk");
 print "Access Token: " . $accessToken . "\n";
 
 $dbxClient = new dbx\Client($accessToken, "Datastoress");
 $accountInfo = $dbxClient->getAccountInfo();
-print_r($accountInfo);
+print_r($accountInfo);*/
 ?>
---
+++ php/recibir.php
@@ -0,0 +1,17 @@
+<?php 
+require_once "dropbox-sdk/Dropbox/autoload.php";
+use \Dropbox as dbx;
+
+$appInfo = dbx\AppInfo::loadFromJsonFile("dropbox-sdk/app-info.json");
+$webAuth = new dbx\WebAuthNoRedirect($appInfo, "Datastoress");
+print_r($_GET["code"]);
+
+$codigo = trim($_GET["code"]."");
+
+list($accessToken, $dropboxUserId) = $webAuth->finish($codigo);
+print "Access Token: " . $accessToken . "\n";
+
+$dbxClient = new dbx\Client($accessToken, "Datastoress");
+$accountInfo = $dbxClient->getAccountInfo();
+print_r($accountInfo);
+?>
