05122913110c5c29870ebd0e8ded85d170eaa7f0
yuranisalamanca <yuranisalamanca@gmail.com>
2015-03-21 17:11:22 -0500

    Inicio login

--- php/lib/users.php
+++ php/lib/users.php
@@ -10,6 +10,18 @@
 			$result = $conexion->query($query);	
 			return $result;
 		}
+
+		function getAutentificacion($usuario, $contrasenia){
+			include 'lib/db_connect.php';
+			$query = "SELECT * FROM users WHERE usuario = ?, contraseña = ?";
+			$result = $conexion->prepare($query);
+			$result->bindParam(1, $usuario);
+			$result->bindParam(2, $contrasenia);
+			$result->execute();
+			if($row = fetch_array($result)){
+				
+			}
+		}
 	}
 	
  ?>
