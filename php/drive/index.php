6eff408bd4940031d17ba1579af55fb12e6ab344
Judaseec <judaseec@hotmail.com>
2015-04-06 14:47:26 -0500

    Conexion con Drive
    Permite subir un archivo local al repositorio de drive

--- php/drive/index.php
+++ php/drive/index.php
@@ -4,17 +4,22 @@ $url_array = explode('?', 'http://'.$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI
 $url = $url_array[0];
 require_once 'google-api-php-client/src/Google_Client.php';
 require_once 'google-api-php-client/src/contrib/Google_DriveService.php';
+
 $client = new Google_Client();
 $client->setClientId('939632221621-12nef07kiggh62irqgcgf6u4glp37cur.apps.googleusercontent.com');
 $client->setClientSecret('NSKrnnvoG5KERS_9BLZZCAEt');
 $client->setRedirectUri($url);
 $client->setScopes(array('https://www.googleapis.com/auth/drive'));
+
 if (isset($_GET['code'])) {
+    print_r($_GET['code']);
     $_SESSION['accessToken'] = $client->authenticate($_GET['code']);
-    header('location:'.$url);exit;
+//    header('location:'.$url);exit;
 } elseif (!isset($_SESSION['accessToken'])) {
     $client->authenticate();
 }
+
+print_r($_SESSION['accessToken']);
 
 $files= array();
 $dir = dir('files');
@@ -28,6 +33,7 @@ if (!empty($_POST)) {
     $client->setAccessToken($_SESSION['accessToken']);
     $service = new Google_DriveService($client);
     $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    print_r(FILEINFO_MIME_TYPE);
     $file = new Google_DriveFile();
     foreach ($files as $file_name) {
         $file_path = 'files/'.$file_name;
