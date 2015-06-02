7f3921a99ad9549f512cf12b85f28c8d371187ec
yuranisalamanca <yuranisalamanca@gmail.com>
2015-06-02 14:42:41 -0500

    Modicacion de estructura de cuenta

--- application/views/cuenta.php
+++ application/views/cuenta.php
@@ -1,71 +1,82 @@
 <div class="cuerpo">
-		<div class="barraCuenta">	
-			<ul class="listabarraprincipal">
-				<li><a class="inicarsesion" href="">Iniciar Sesi&oacute;n</a></li>
-			</ul>
-		</div>
+	<div class="barraCuenta">	
+		<ul class="listabarraprincipal">
+			<li><a class="inicarsesion" href="">Iniciar Sesi&oacute;n</a></li>
+		</ul>
+	</div>
 
-		<div class="cuerpoCuenta">
-			<form method="post" action="<?php echo site_url('usuario/createUser') ?>" class="cuentaForm">
-				<fieldset>
-					<legend>Crea tu cuenta</legend>
-					<?php if (isset($_POST['exitoAgregar'])){ ?>
+	<div class="cuerpoCuenta">
+		<form method="post" action="<?php echo site_url('usuario/createUser') ?>" class="cuentaForm">
+			<fieldset>
+				<legend>Crea tu cuenta</legend>
+				<?php if (isset($_POST['exitoAgregar'])){ ?>
+			
+				<div class="alert alert-danger" role="alert">
+					<?php echo $_POST['exitoAgregar']; ?>
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+	  					<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<?php } ?>
 				
-					<div class="alert alert-danger" role="alert">
-						<?php echo $_POST['exitoAgregar']; ?>
-						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-		  					<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<?php } ?>
-					
-					<div class="generalCuenta">
-						<div class="leftCuenta">
-							<label class="etiquetasFormCuenta"> Nombre 
-								<input class="labelCuenta" name="nombreU" type="text" placeholder="Escriba su nombre" required>
-							</label>
-
-							<label class="etiquetasFormCuenta"> Correo Electr&oacute;nico 
-								<input class="labelCuenta" name="correoU" type="email" placeholder="Escriba su correo electr&oacute;nico" required>
-							</label>
-
-							<label class="etiquetasFormCuenta"> Contrase&ntilde;a 
-								<input type="password" class="labelCuenta" name="contraseniaU" placeholder="Escriba su contrase&ntilde;a" required>
-							</label>
-						</div>
-
-						<div class="rightCuenta">
-							<label class="etiquetasFormCuenta"> Apellido
-								<input class="labelCuenta" name="apellidoU" type="text" placeholder="Escriba sus apellidos" required>
-							</label>
-							<label class="etiquetasFormCuenta"> Usuario
-								<input class="labelCuenta" name="usuarioU" type="text" placeholder="Escriba el nombre de usuario" required>
-							</label>
-							
-							<label class="etiquetasFormCuenta"> Confirma tu contrase&ntilde;a 
-								<input type="password" class="labelCuenta" name="contraseniaConfirmU" placeholder="Escriba su contrase&ntilde;a" required>
+				<div class="generalCuenta">
+					<div class="leftCuenta">
+						<label class="etiquetasFormCuenta"> Nombre 
+							<input class="labelCuenta" name="nombreU" type="text" placeholder="Escriba su nombre" required>
 						</label>
-						</div>
+
+						<label class="etiquetasFormCuenta"> Correo Electr&oacute;nico 
+							<input class="labelCuenta" name="correoU" type="email" placeholder="Escriba su correo electr&oacute;nico" required>
+						</label>
+
+						<label class="etiquetasFormCuenta"> Contrase&ntilde;a 
+							<input type="password" class="labelCuenta" name="contraseniaU" placeholder="Escriba su contrase&ntilde;a" required>
+						</label>
 					</div>
-					<input type="submit" class="buttonCrearCuenta" value="Crear cuenta">
-				</fieldset>
-				
+
+					<div class="rightCuenta">
+						<label class="etiquetasFormCuenta"> Apellido
+							<input class="labelCuenta" name="apellidoU" type="text" placeholder="Escriba sus apellidos" required>
+						</label>
+						<label class="etiquetasFormCuenta"> Usuario
+							<input class="labelCuenta" name="usuarioU" type="text" placeholder="Escriba el nombre de usuario" required>
+						</label>
+						
+						<label class="etiquetasFormCuenta"> Confirma tu contrase&ntilde;a 
+							<input type="password" class="labelCuenta" name="contraseniaConfirmU" placeholder="Escriba su contrase&ntilde;a" required>
+					</label>
+					</div>
+				</div>
+				<input type="submit" class="buttonCrearCuenta" value="Crear cuenta">
+			</fieldset>
+		</form>
+			
+		<form method="post" class="cuentaForm">
+			<fieldset>
 				<div class="infoDrive">
 					<div class="logos">
 						<h3 class="tituloEmpresa">Drive</h3>
 						<img src="<?php echo base_url(); ?>fonts/img/google-drive-logo.png">
 					</div>
-					<input type="submit" class="buttonCrearCuenta" value="Aceptar Terminos y Condiciones en Drive">
 				</div>
+			</fieldset>
+			<input type="submit" class="buttonCrearCuenta" value="Aceptar Terminos y Condiciones en Drive">
+		</form>
 
+		<form method="post" class="cuentaForm">
+			<fieldset>
 				<div class="infoDropBox">
 					<div class="logos">
 						<h3 class="tituloEmpresa">Drive</h3>
 						<img src="<?php echo base_url(); ?>fonts/img/dropbox-logo.png">
 					</div>
-					<input type="submit" class="buttonCrearCuenta" value="Aceptar Terminos y Condiciones en Dropbox">
 				</div>
+			</fieldset>
+			<input type="submit" class="buttonCrearCuenta" value="Aceptar Terminos y Condiciones en Dropbox">
+		</form>
 
+		<form method="post" class="cuentaForm">
+			<fieldset>
 				<div class="infoMega">
 					<div class="logos">
 						<h3 class="tituloEmpresa">Mega</h3>
@@ -90,10 +101,9 @@
 					</div>
 				</div>
 				<input type="submit" class="buttonCrearCuenta" value="Aceptar Terminos y Condiciones en Mega">
-
-		
-			</form>
-		</div>
+			</fieldset>
+		</form>
+	</div>
 
 </div>
 			
